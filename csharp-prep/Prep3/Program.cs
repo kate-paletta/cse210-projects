f0d9457fd9760f2ffd76ca102b3847afe1001510
Kate Paletta <kate.f.knox@gmail.com>
2023-01-16 10:00:57 -0700

    magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randonGenerator = new Random();
+        int magicNum = randonGenerator.Next(1, 101);
+
+        int userNum = -1;
+
+        while (userNum != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            userNum = int.Parse(Console.ReadLine());
+
+            if (magicNum > userNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNum < userNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
