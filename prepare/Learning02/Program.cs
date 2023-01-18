5ae88a3dd90a05ea93d84183cd2815251df614f4
Kate Paletta <kate.f.knox@gmail.com>
2023-01-18 16:20:23 -0700

    started learning 2 project

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Web Developer";
+        job1._company = "Microsoft";
+        job1._startYear = 2018;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Full Stack Developer";
+        job2._company = "Accumen";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        job1.Display();
+        job2.Display();
+
+        Resume resume1 = new Resume();
     }
 }
