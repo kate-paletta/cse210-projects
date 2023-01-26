5c40b11b26a8b216458b22bfb735a609e1683638
Kate Paletta <kate.f.knox@gmail.com>
2023-01-26 11:58:52 -0700

    created menu text

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.Write("Please select one of the following choices");
+        Console.Write("1.Write");
+        Console.Write("2.Display");
+        Console.Write("3.Load");
+        Console.Write("4.Save");
+        Console.Write("5.Quit");
+        Console.Write("What would you like to do?");
+
     }
 }
